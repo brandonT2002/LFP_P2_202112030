0b27e978d459be6401a97327481bcbb85cca6a0e
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2022-12-26 22:32:54 -0600

    reading files from interface

--- Interface/OptionPane.py
+++ Interface/OptionPane.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import *
+from tkinter.filedialog import askopenfilename
+from Backend.Controller import Controller
 
 class OptionPane(tk.Frame):
     def __init__(self,root,grammar,automaton):
@@ -7,6 +9,7 @@ class OptionPane(tk.Frame):
         self.configure(bg='#2A2D2E')
         self.grid(row=0,column=0,sticky='nswe')
 
+        self.ctrl = Controller()
         self.grammar = grammar
         self.automaton = automaton
         self.components()
@@ -20,7 +23,7 @@ class OptionPane(tk.Frame):
         opciones = Label(master=self,text='Opciones',font=('Roboto Medium',16),background='#2A2D2E',foreground='white')
         opciones.grid(row=1,column=0,pady=10,padx=10)
 
-        self.upload = Button(master=self,text='Cargar Archivo',font=('Roboto Medium',11),bg='#0059b3',activebackground='#0059b3',foreground='white',activeforeground='white',width=15,height=1,cursor='hand2')
+        self.upload = Button(master=self,text='Cargar Archivo',font=('Roboto Medium',11),bg='#0059b3',activebackground='#0059b3',foreground='white',activeforeground='white',width=15,height=1,cursor='hand2',command=self.chooseFile)
         self.upload.grid(row=2,column=0,pady=10,padx=20)
 
         self.grammarMod = Button(master=self,text='Modulo Gramática',font=('Roboto Medium',11),bg='#0059b3',activebackground='#0059b3',foreground='white',activeforeground='white',width=15,height=1,cursor='hand2',command=self.option1)
@@ -31,6 +34,30 @@ class OptionPane(tk.Frame):
 
         self.exit = Button(master=self,text='Salir',font=('Roboto Medium',11),bg='#D35B58',activebackground='#D35B58',foreground='white',activeforeground='white',width=15,height=1,cursor='hand2',command=quit)
         self.exit.grid(row=9,column=0,pady=10,padx=20)
+
+    def chooseFile(self):
+        try:
+            formatos = (
+                ("form files","*.glc"),
+                ("form files","*.apl"),
+            )
+            archivo = askopenfilename(
+                title='Abrir Archivo',
+                initialdir='',
+                filetypes = formatos)
+            if not archivo == '':
+                extension = archivo.split('.')
+                if extension[1] == 'glc':
+                    self.ctrl.readFileGLC(archivo)
+                    self.ctrl.grammarRecognition()
+                    print('--Gramaticas--')
+                    self.ctrl.showGrammar()
+                elif extension[1] == 'apl':
+                    self.ctrl.readFileAPL(archivo)
+                    self.ctrl.automatonRecognition()
+                    print('--Automatas--')
+                    self.ctrl.showAutomaton()
+        except: pass
 
     def option1(self):
         self.automaton.grid_remove()
--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 from Interface.MainWindow import MainWindow
 from Backend.Controller import Controller
 from Backend.Graph import dotReports
-ctrl = Controller()
-ctrl.readFileGLC('Gramatica.glc')
-ctrl.grammarRecognition()
-print('---GRAMATICAS LIBRES DE CONTEXTO---')
-#ctrl.showGrammar()
-
-ctrl.readFileAPL('Automata.apl')
-ctrl.automatonRecognition()
-print('---AUTOMATAS DE PILA---')
-#ctrl.showAutomaton()
 
 #print(ctrl.stackAutomata[0])
-gr = dotReports()
-gr.generateSAReport(ctrl.stackAutomata[0])
+#gr = dotReports()
+#gr.generateSAReport(ctrl.stackAutomata[0])
 
 window = MainWindow()
